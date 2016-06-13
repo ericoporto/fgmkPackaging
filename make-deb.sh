790f65c930b77bb968325b807c3ff72220d1505e
ericoporto <ericoporto2008@gmail.com>
2016-06-13 20:07:32 -0300

    updated to use MANIFEST.in

--- make-deb.sh
+++ make-deb.sh
@@ -7,4 +7,5 @@ git clone https://github.com/ericoporto/fgmk.git
 cd fgmk
 cp -rf $here/ubuntu-related/data .
 cp -f $here/ubuntu-related/setup.py .
+cp -f $here/ubuntu-related/MANIFEST.in .
 python3 setup.py --command-packages=stdeb.command bdist_deb
